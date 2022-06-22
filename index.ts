1b53fd78c881e07c057a19d31dbfc865b36a450e
Nelis Junior <contato@nelis.me>
2022-06-21 21:43:00 -0300

    aula 04: n33gBIiJPVc

--- index.ts
+++ index.ts
@@ -1,4 +1,26 @@
-function soma(a: number, b: number){
-    return a + b;
+// Types
+// Interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+    executarRugido(alturaEmDecibeis: number): void;
 }
 
+interface IFelino extends IAnimal {
+    visaoNoturn: boolean;
+}
+
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+animal.executarRugido('s');
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturn: true,
+}
